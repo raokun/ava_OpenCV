15d7eca40613667c445778b28ec1a5c712d050e6
raokun <272728816@qq.com>
2023-11-16 21:05:25 +0800

    update
    update

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ava_OpenCV
-基于avalonia UI和OpenCV的图片处理软件
+基于avalonia UI和OpenCvSharp的图片处理软件
+
+[OpenCvSharp 文档](https://shimat.github.io/opencvsharp_docs/html)
+
+[github文档](http://shimat.github.io/opencvsharp/api/)
--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using ava_OpenCV.Views;
@@ -8,8 +9,11 @@ using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Media;
 using Avalonia.Media.Imaging;
 using OpenCvSharp;
+using OpenCvSharp.Dnn;
 using OpenCvSharp.Extensions;
 using ReactiveUI;
+using Point = System.Drawing.Point;
+using Size = System.Drawing.Size;
 
 namespace ava_OpenCV.ViewModels;
 
@@ -133,7 +137,91 @@ public class MainWindowViewModel : ViewModelBase
 
     private void Test()
     {
-        WorkMat.Resize(2);
+        // WorkMat.Resize(2);
+        // var bmp = WorkMat.ToMemoryStream();
+        // OutPutImageSource = new Bitmap(bmp);
+        HandPose();
+    }
+    /// <summary>
+    /// 灰白色调
+    /// </summary>
+    private void GrayColor()
+    {
+        Cv2.CvtColor(WorkMat,WorkMat,ColorConversionCodes.BGR2GRAY, 0);
+        var bmp = WorkMat.ToMemoryStream();
+        OutPutImageSource = new Bitmap(bmp);
+    }
+
+    const string model = "pose_iter_102000.caffemodel";
+    const string modelTxt = "pose_deploy.prototxt";
+    const string sampleImage = "hand.jpg";
+    const string outputLoc = "Output_Hand.jpg";
+    const int nPoints = 22;
+    const double thresh = 0.01;
+    
+    private void HandPose()
+    {
+        if (WorkMat == null)
+        {
+            WorkMat = new Mat(FilePath, ImreadModes.Unchanged);
+        }
+        int[][] posePairs =
+        {
+            new[] {0, 1}, new[] {1, 2}, new[] {2, 3}, new[] {3, 4}, //thumb
+            new[] {0, 5}, new[] {5, 6}, new[] {6, 7}, new[] {7, 8}, //index
+            new[] {0, 9}, new[] {9, 10}, new[] {10, 11}, new[] {11, 12}, //middle
+            new[] {0, 13}, new[] {13, 14}, new[] {14, 15}, new[] {15, 16}, //ring
+            new[] {0, 17}, new[] {17, 18}, new[] {18, 19}, new[] {19, 20}, //small
+        };
+        int frameWidth = WorkMat.Cols;
+        int frameHeight = WorkMat.Rows;
+        float aspectRatio = frameWidth / (float) frameHeight;
+        int inHeight = 368;
+        int inWidth = ((int) (aspectRatio * inHeight) * 8) / 8;
+        using var net = CvDnn.ReadNetFromCaffe(modelTxt, model);
+        using var inpBlob = CvDnn.BlobFromImage(_workMat, 1.0 / 255, new OpenCvSharp.Size(inWidth, inHeight),
+            new Scalar(0, 0, 0), false, false);
+
+        net.SetInput(inpBlob);
+        using var output = net.Forward();
+        int H = output.Size(2);
+        int W = output.Size(3);
+
+        var points = new List<OpenCvSharp.Point>();
+
+        for (int n = 0; n < nPoints; n++)
+        {
+            // Probability map of corresponding body's part.
+            using var probMap = new Mat(H, W, MatType.CV_32F, output.Ptr(0, n));
+            Cv2.Resize(probMap, probMap, new OpenCvSharp.Size(frameWidth, frameHeight));
+            Cv2.MinMaxLoc(probMap, out _, out var maxVal, out _, out OpenCvSharp.Point maxLoc);
+
+            if (maxVal > thresh)
+            {
+                Cv2.Circle(WorkMat, maxLoc.X, maxLoc.Y, 8, new Scalar(0, 255, 255), -1,
+                    LineTypes.Link8);
+                Cv2.PutText(WorkMat, Cv2.Format(n), new OpenCvSharp.Point(maxLoc.X, maxLoc.Y),
+                    HersheyFonts.HersheyComplex, 1, new Scalar(0, 0, 255), 2, LineTypes.AntiAlias);
+            }
+
+            points.Add(maxLoc);
+        }
+
+        int nPairs = 20; //(POSE_PAIRS).Length / POSE_PAIRS[0].Length;
+
+        for (int n = 0; n < nPairs; n++)
+        {
+            // lookup 2 connected body/hand parts
+            OpenCvSharp.Point partA = points[posePairs[n][0]];
+            OpenCvSharp.Point partB = points[posePairs[n][1]];
+
+            if (partA.X <= 0 || partA.Y <= 0 || partB.X <= 0 || partB.Y <= 0)
+                continue;
+
+            Cv2.Line(WorkMat, partA, partB, new Scalar(0, 255, 255), 8);
+            Cv2.Circle(WorkMat, partA.X, partA.Y, 8, new Scalar(0, 0, 255), -1);
+            Cv2.Circle(WorkMat, partB.X, partB.Y, 8, new Scalar(0, 0, 255), -1);
+        }
         var bmp = WorkMat.ToMemoryStream();
         OutPutImageSource = new Bitmap(bmp);
     }
